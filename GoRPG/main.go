05fab4beb5a8a364cf87f6f0d1da38a741da3e0d
Ace120C <145904614+Ace120C@users.noreply.github.com>
2024-10-10 11:28:03 +0100

    Add files via upload

--- GoRPG/main.go
+++ GoRPG/main.go
@@ -9,27 +9,33 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{
-  PlayerImage *ebiten.Image
+
+type Sprite struct{
+  Img *ebiten.Image
   X, Y float64
+}
+
+type Game struct{
+  player *Sprite
+  sprites []*Sprite
 }
 
 func (g *Game) Update() error {
   //react to key presses
   if ebiten.IsKeyPressed(ebiten.KeyRight) {
-    g.X += 2 
+    g.player.X += 2 
   }
 
   if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-    g.X -= 2 
+    g.player.X -= 2 
   }
 
   if ebiten.IsKeyPressed(ebiten.KeyUp) {
-    g.Y -= 2 
+    g.player.Y -= 2 
   }
 
   if ebiten.IsKeyPressed(ebiten.KeyDown) {
-    g.Y += 2 
+    g.player.Y += 2 
   }
 
 
@@ -41,14 +47,30 @@ func (g *Game) Draw(screen *ebiten.Image) {
   screen.Fill(color.RGBA{120, 180, 255, 255})
 
   opts := ebiten.DrawImageOptions{}
-  opts.GeoM.Translate(g.X, g.Y)
+  opts.GeoM.Translate(g.player.X, g.player.Y)
   // draw the Player
   screen.DrawImage(
-    g.PlayerImage.SubImage(
+    g.player.Img.SubImage(
       image.Rect(0, 0, 16, 16),
       ).(*ebiten.Image),
       &opts,
     )
+
+  opts.GeoM.Reset()
+
+  for _, sprite := range g.sprites{
+    opts.GeoM.Translate(sprite.X, sprite.Y)
+
+    screen.DrawImage(
+      sprite.Img.SubImage(
+      image.Rect(0, 0, 16, 16),
+      ).(*ebiten.Image),
+      &opts,
+
+    )
+
+    opts.GeoM.Reset()
+  }
 
 	//ebitenutil.DebugPrint(screen, "Hello, World!")
 }
@@ -60,7 +82,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
   //default is 640x480
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("RPG Game Test")
   ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
   PlayerImg, _, err := ebitenutil.NewImageFromFile("/home/ace/Golang/GoRPG/Assets/Images/ninja.png")
@@ -68,7 +90,28 @@ func main() {
     //handle the error
     log.Fatal(err)
   }
-  if err := ebiten.RunGame(&Game{PlayerImage: PlayerImg, X: 100, Y: 100}); err != nil {
+  SkeletonImg, _, err := ebitenutil.NewImageFromFile("/home/ace/Golang/GoRPG/Assets/Images/skeleton.png")
+  if err != nil {
+    //handle the error
+    log.Fatal(err)
+  }
+
+  
+  Game := Game {
+    player: &Sprite{
+      Img: PlayerImg,
+      X: 50.0,
+      Y: 50.0,
+    },
+    sprites: []*Sprite {
+      &Sprite{
+        Img: SkeletonImg,
+        X: 100.0,
+        Y: 100.0,
+      },
+    },
+  }
+  if err := ebiten.RunGame(&Game); err != nil {
 		log.Fatal(err)
 	}
 }
